8cd556caec95156d74f48eeb364a2e72b70c64df
aakash-it19 <aakash.it19@bitsathy.ac.in>
2019-08-28 08:48:52 +0530

    second commit

---
+++ bio.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<body>
+<html>
+
+<head>
+
+<title>
+
+BIO-DATA
+
+</title>
+
+<body>
+
+<table border="1" width="600" align="center" bgcolor="salmon">
+
+<caption align="center">BIO-DATA</caption>
+
+<tr>
+
+<td colspan="2" align="center">
+
+</td>
+
+</tr>
+
+</table>
+
+<table border="1" width="600" align="center" bgcolor="white">
+
+<tr> <font face="verdana" color="red">
+
+<td>NAME:</td>
+
+<td><font color="black">S.AAKASH</font></td>
+
+</font>
+
+</tr>
+
+<tr> <font face="verdana">
+
+<td colspan="2" align="center" height="40" bgcolor="lightgreen"><font color="WHITE"><b>ADDRESS</b></font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>CITY:</td>
+
+<td><font color="BLACK">COONOOR</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>STATE:</td>
+
+<td><font color="BLACK">TAMILNADU</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>PIN-CODE</td>
+
+<td><font color="BLACK">643102</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>EMAIL:</td>
+
+<td><font color="BLACK">aakash.it19@bitsathy.ac.in</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>PHONE:</td>
+
+<td><font color="BLACK">9944769396</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>MOBILE NO.:</td>
+
+<td><font color="BLACK">9080205800</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>D.O.B:</td>
+
+<td><font color="BLACK">15-08-2001</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>GENDER:</td>
+
+<td><font color="BLACK">Male</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>COLLEGE:</td>
+
+<td><font color="BLACK">BANNARI AMMAN INSTITUTION OF TECHNOLOGY</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>DEPARTMENT:</td>
+
+<td><font color="BLACK">IT</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td colspan="2" align="center" height="40" bgcolor="lightcoral"><font color="WHITE"><b>EDUCATIONAL QUALIFICATION</b></font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>10th</td>
+
+<td><font color="BLACK">72</font></td>
+
+</font>
+
+</tr>
+
+<tr>
+
+<font face="verdana">
+
+<td>12th</td>
+
+<td><font color="BLACK">60</font></td>
+
+</font>
+
+</tr>
+<tr width="30">
+<font face="verdana">
+<td>EXTRA ACTIVITY:</td>
+<td><font color="BLACK">Cricket,Football</font></td>
+</font>
+</tr>
+<tr width="30">
+<font face="verdana">
+<td>PROFESSIONAL QUALIFICATION:</td><td><font color="BLACK">C,C++,,Html,Php</font></td>
+
+</font>
+
+</tr>
+
+
+<tr width="30">
+
+<font face="verdana">
+
+<td>HOBBY:</td>
+
+<td><font color="BLACK">Listening Music Reading books Football,Playing Cricket</font></td>
+
+</font>
+
+</tr>
+
+</table>
+
+</body>
+
+</html>
+
+</body>
+</html>
+
+
+
